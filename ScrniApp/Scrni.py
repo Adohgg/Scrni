7c44a0f67789bc7d6ebcd9d587747cb489343f8d
Justin Hofbauer <justin.hofbauer@rogers.com>
2013-11-04 13:47:47 -0500

    Change 'temp.png' to 'penis.png'
    Optimization.

--- ScrniApp/Scrni.py
+++ ScrniApp/Scrni.py
@@ -13,7 +13,7 @@ class ImageHandle():
         self.selection = Region.Selection(self)
 
     def capture_desktop(self, event):
-        im=ImageGrab.grab_to_file("temp.png")
+        im=ImageGrab.grab_to_file("penis.png")
         self.post_image()
 
     def select_region(self, event):
@@ -23,12 +23,12 @@ class ImageHandle():
         p1, p2 = self.fixCoords(c1, c2)
         #print "Point 1 (" + str(p1.x) + "," + str(p1.y) + ")"
         #print "Point 2 (" + str(p2.x) + "," + str(p2.y) + ")"
-        im=ImageGrab.grab(bbox=(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y)).save("temp.png")
+        im=ImageGrab.grab(bbox=(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y)).save("penis.png")
         self.post_image()
 
     def post_image(self):
         register_openers()
-        datagen, headers = multipart_encode({"img": open("temp.png", "rb")})
+        datagen, headers = multipart_encode({"img": open("penis.png", "rb")})
         request = urllib2.Request("http://162.243.65.219/upload_image.php", datagen, headers)
         TrayIcon.copyResponse(self.taskicon, urllib2.urlopen(request).read())
 
